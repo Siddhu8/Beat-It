f656cfa235560a4c9f458d578bfe7c6e5983ac33
Prashanth <prashanthgadipelli@gmail.com>
2020-04-10 17:31:25 +0530

    updated

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.static('public'))
-app.get('/', (req, res) => res.sendFile(__dirname + '/public/html/resume.html'))
+app.use(express.static(__dirname + '/BEAT-IT'));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirnmae + '/BEAT-IT/index.html')))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
